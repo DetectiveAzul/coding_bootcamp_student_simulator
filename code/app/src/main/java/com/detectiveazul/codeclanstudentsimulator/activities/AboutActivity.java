85384997b7dfc9e197f25b251948d2308ef47214
Jaime Lopez <detec.azul@gmail.com>
2018-05-01 14:51:04 +0100

    About App Activity finished

--- code/app/src/main/java/com/detectiveazul/codeclanstudentsimulator/activities/AboutActivity.java
+++ code/app/src/main/java/com/detectiveazul/codeclanstudentsimulator/activities/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.detectiveazul.codeclanstudentsimulator.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.detectiveazul.codeclanstudentsimulator.R;
 
@@ -14,5 +17,18 @@ public class AboutActivity extends AppCompatActivity {
 
         //Changing the action bar title
         getSupportActionBar().setTitle(R.string.menu_about);
+    }
+
+    public void onGitHubClick(View view) {
+        openWebURL("https://github.com/DetectiveAzul/codeclan_student_simulator");
+    }
+
+    public void onFlatIconClick(View view) {
+        openWebURL("https://www.flaticon.com/authors/ddara");
+    }
+
+    private void openWebURL(String url) {
+        Intent browse = new Intent( Intent.ACTION_VIEW , Uri.parse(url) );
+        startActivity( browse );
     }
 }
