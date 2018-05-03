94813335015258fc5fffd859b4e2d855a76bde21
Jaime Lopez <detec.azul@gmail.com>
2018-05-03 15:22:31 +0100

    Fixing string swapping on end scene string

--- code/app/src/main/java/com/detectiveazul/codeclanstudentsimulator/model/Constants/Difficult.java
+++ code/app/src/main/java/com/detectiveazul/codeclanstudentsimulator/model/Constants/Difficult.java
@@ -5,9 +5,9 @@ import com.detectiveazul.codeclanstudentsimulator.R;
 public enum Difficult {
     NEUTRAL(R.string.neutral, 0),
     EASY(R.string.easy, 10),
-    MEDIUM(R.string.medium, 25),
-    HARD(R.string.hard, 20),
-    EXTRA_HARD(R.string.extra_hard, 30);
+    MEDIUM(R.string.medium, 15),
+    HARD(R.string.hard, 25),
+    EXTRA_HARD(R.string.extra_hard, 35);
 
     private final int prettyName;
     private final int level;
